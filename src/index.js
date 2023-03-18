ac1bc3c98656b28d6d1b6dc6a281d1b475aa6d12
Igor-Novitski <pro100nke@gmail.com>
2023-03-18 17:59:25 +0300

    feat: functional to button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import './index.css';
 
-console.log(2);
+const cards = document.querySelectorAll('.card');
+const button = document.querySelector('.button');
+
+function changeClassActive() {
+  if (!this.classList.contains('active')) {
+    cards.forEach((item) => {
+      if (item.classList.contains('active')) {
+        item.classList.remove('active');
+      } else {
+        item.classList.add('active');
+      }
+    });
+  }
+}
+
+function followTheLink() {
+  if (cards[0].classList.contains('active')) {
+    location.href = 'https://apple.com/';
+  } else {
+    location.href = 'https://google.com/ ';
+  }
+}
+
+cards.forEach((card) => {
+  card.addEventListener('click', changeClassActive);
+});
+
+button.addEventListener('click', followTheLink);
